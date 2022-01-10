2e285f7e7395e78fefc75f47cc3fb62d04b13e38
Patrick <patrick.lemke@espero.tech>
2022-01-10 14:57:27 +0100

    project setup

---
+++ app/src/main/java/com/nimmsta/nimmsta_kotlin_android_example_project/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.nimmsta.nimmsta_kotlin_android_example_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.nimmsta.core.android.framework.NIMMSTAServiceConnection
+import com.nimmsta.core.shared.device.NIMMSTADevice
+import com.nimmsta.core.shared.device.NIMMSTAEventHandler
+
+class MainActivity : AppCompatActivity(), NIMMSTAEventHandler {
+
+    /* ... */
+    private lateinit var serviceConnection: NIMMSTAServiceConnection
+    /* ... */
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        serviceConnection = NIMMSTAServiceConnection.bindServiceToActivity(this).onComplete {
+            try {
+                // this is the point in time when the (background) task completes and the result throws if an error occurred.
+                it.result
+
+                it.result.enableBackgroundAndNotifications()
+
+                // Check if connected, if not show connection Activity
+                if (!serviceConnection.isConnected) {
+                    serviceConnection.displayConnectionActivity()
+                }
+            } catch (throwable: Throwable) {
+                throwable.printStackTrace()
+
+                Toast.makeText(
+                    this@MainActivity,
+                    throwable.message,
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    override fun didConnectAndInit(device: NIMMSTADevice) {
+        // this is the point in time when you can start interacting with your device.
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        serviceConnection.close()
+    }
+}
